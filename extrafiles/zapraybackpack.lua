7507b7998d56657c696140446b9e939b9e59499c
myluascripts <junichips1@gmail.com>
2025-02-19 13:35:46 -0700

    Update zapraybackpack.lua

--- extrafiles/zapraybackpack.lua
+++ extrafiles/zapraybackpack.lua
@@ -1,7 +1,7 @@
 local contestants = workspace.Contestants:GetChildren()
 for _, contestant in ipairs(contestants) do
     if contestant:FindFirstChild("Torso") then
-        game.Players.LocalPlayer.Backpack["Zap Ray"].ZapShoot:FireServer(workspace.Contestants[contestant].Torso.Position)
+        game.Players.LocalPlayer.Backpack["Zap Ray"].ZapShoot:FireServer(contestant.Torso.Position)
         wait(0.05)
     end
 end
