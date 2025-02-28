d8366630e53a760bbabae2bb37b99b8ae2100c45
myluascripts <junichips1@gmail.com>
2025-02-28 15:19:03 -0700

    Update and rename infinite-yield-(not-mine).lua to infinite-yield-(loads-official-script).lua

---
+++ infinite-yield-(loads-official-script).lua
@@ -0,0 +1,9 @@
+StarterGui:SetCore("SendNotification", {
+  Title = "hi", -- Notification title
+  Text = "i didn't make infinite yield, i created this script to load infinite yield without me having to click out of roblox", -- Notification text
+  Icon = "", -- Notification icon (optional)
+  Duration = 5, -- Duration of the notification (optional, may be overridden if more than 3 notifs appear)
+  Callback = nil, -- Bindable Event (will provide docs if wanted)
+  Button1 = "OK", -- button 1 text (optional)
+})
+loadstring(game:HttpGet("https://infyiff.github.io/resources/IY_FE.txt", true))()
--- infinite-yield-(not-mine).lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://infyiff.github.io/resources/IY_FE.txt", true))()
